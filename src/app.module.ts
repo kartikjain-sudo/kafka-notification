c6c0c8e01cf838b48d4ba3b2e0d90f0ea408bbc7
kartikjain-sudo <kartikjain42@protonmail.com>
2023-01-18 11:31:04 +0530

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [],
+  imports: [
+    // ClientsModule.register([
+    //   {
+    //     name: 'REGISTRATION_SERVICE',
+    //     transport: Transport.KAFKA,
+    //     options: {
+    //       client: {
+    //         clientId: 'registration',
+    //         brokers: ['localhost:9092'],
+    //       },
+    //       consumer: {
+    //         groupId: 'REGISTRATION_SERVICE_GROUP',
+    //       },
+    //     },
+    //   },
+    // ]),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.host,
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.user,
+          pass: process.env.pass,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly mailerService: MailerService,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async sendMail(data: any): Promise<boolean> {
+    const { email } = data;
+    
+    const mailOptions = {
+      email,
+      from: 'kj@rapidinnovation.dev',
+      subject: 'Welcome mail',
+      text: 'Welcome to the GHC',
+  };
+  
+  await this.mailerService.sendMail(mailOptions);
+
+    return true;
   }
 }
